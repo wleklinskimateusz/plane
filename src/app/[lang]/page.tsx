80ad602f8866c93efdfb3d3d1849724d036418ed
wleklinskimateusz <wleklinskimateusz05@gmail.com>
2025-05-07 22:49:18 +0200

    feat: add join page

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -87,7 +87,7 @@ export default async function Home({
         </div>
       </section>
 
-      <section id="join" className="py-24 bg-black">
+      <section id="join" className="py-24 bg-gray-800">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="text-center mb-16">
             <h2 className="text-4xl font-bold text-white mb-4">{join.title}</h2>
